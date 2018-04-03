8adfa8528711171010256a5a09fae72adf9782e2
xsistema <30940645+xsistema@users.noreply.github.com>
2018-04-03 11:00:34 +0300

    Update hash-extra-skein.c
    Compiler warning: unused variable ‘r’ [-Wunused-variable]

--- src/crypto/hash-extra-skein.c
+++ src/crypto/hash-extra-skein.c
@@ -10,6 +10,5 @@
 #include "skein.h"
 
 void hash_extra_skein(const void *data, size_t length, char *hash) {
-  int r = skein_hash(8 * HASH_SIZE, data, 8 * length, (uint8_t*)hash);
-  assert(SKEIN_SUCCESS == r);
+  assert(SKEIN_SUCCESS == skein_hash(8 * HASH_SIZE, data, 8 * length, (uint8_t*)hash));
 }
